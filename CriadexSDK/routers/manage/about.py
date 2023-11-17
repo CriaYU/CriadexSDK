357b1ca8271edeb6d87eaed6cfbfc9a7a7302494
Isaac Kogan <isaacikogan@gmail.com>
2023-11-16 22:14:26 -0500

    Update about to be typed

--- CriadexSDK/routers/manage/about.py
+++ CriadexSDK/routers/manage/about.py
@@ -1,12 +1,23 @@
+from datetime import datetime
 from typing import Optional
 
+from pydantic import BaseModel
+
 from CriadexSDK.core.api.route import Route, BaseResponse, outputs
+
+
+class IndexInfo(BaseModel):
+    id: int
+    name: str
+    type: int
+    llm_model_id: int
+    embedding_model_id: int
 
 
 class IndexAboutRoute(Route):
 
     class Response(BaseResponse):
-        info: Optional[dict]
+        info: Optional[IndexInfo]
 
     @outputs(Response)
     async def execute(self, index_name: str) -> Optional[dict]:
