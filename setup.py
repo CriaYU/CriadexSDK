e660ce5cfb1bdf5cde86a2a5c3a6db3f381cbbf5
isaackogan <isaacikogan@gmail.com>
2023-12-24 18:09:56 -0500

    Update Versioning

--- setup.py
+++ setup.py
@@ -20,7 +20,6 @@ setuptools.setup(
         "Topic :: Software Development :: Build Tools",
         "License :: OSI Approved :: MIT License",
         "Natural Language :: English",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
