06a34b47db738932f46cca89e7d79e6e46a4c8b8
isaackogan <isaacikogan@gmail.com>
2023-12-25 19:34:02 -0500

    Fix capitalization of stacktrace header val

--- CriadexSDK/core/network.py
+++ CriadexSDK/core/network.py
@@ -19,7 +19,7 @@ timeout: httpx.Timeout = httpx.Timeout(
 
 def create_httpx_client(error_stacktrace: bool, **kwargs) -> httpx.AsyncClient:
     return httpx.AsyncClient(
-        headers={**kwargs.pop("headers", dict()), "x-api-stacktrace": str(error_stacktrace)},
+        headers={**kwargs.pop("headers", dict()), "x-api-stacktrace": str(error_stacktrace).lower()},
         limits=limits,
         timeout=timeout,
         **kwargs
