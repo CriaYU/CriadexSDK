54597edd136cec23bbac3b7945a5c45725e1d552
isaackogan <isaacikogan@gmail.com>
2024-01-21 18:05:09 -0500

    Fix v1.1.1

--- CriadexSDK/routers/agents/__init__.py
+++ CriadexSDK/routers/agents/__init__.py
@@ -4,6 +4,7 @@ from CriadexSDK.core.api.router import Router
 from CriadexSDK.routers.agents.chat import AgentChatRoute
 from CriadexSDK.routers.agents.intents import AgentIntentsRoute
 from CriadexSDK.routers.agents.lang import AgentLanguageRoute
+from CriadexSDK.routers.agents.rerank import AgentRerankRoute
 
 
 class AgentRouter(Router):
@@ -14,6 +15,7 @@ class AgentRouter(Router):
         self.chat: AgentChatRoute = self._create_route(AgentChatRoute)
         self.intents: AgentIntentsRoute = self._create_route(AgentIntentsRoute)
         self.language: AgentLanguageRoute = self._create_route(AgentLanguageRoute)
+        self.rerank: AgentRerankRoute = self._create_route(AgentRerankRoute)
 
 
 __all__ = ["AgentRouter"]
