63ac1f3e1d75c55d67f4f58217a09cdd95294ee5
isaackogan <isaacikogan@gmail.com>
2024-04-24 17:19:45 -0400

    Swap uploads from file to JSON

--- CriadexSDK/routers/content/upload.py
+++ CriadexSDK/routers/content/upload.py
@@ -6,10 +6,10 @@ from pydantic import BaseModel
 from CriadexSDK.core.api.route import Route, BaseResponse, outputs
 
 
-class UploadFile(BaseModel):
+class ContentUploadConfig(BaseModel):
     file_name: str
-    file_bytes: bytes
-    file_mimetype: str
+    file_contents: dict
+    file_metadata: dict
 
 
 class GroupContentUploadRoute(Route):
@@ -23,14 +23,12 @@ class GroupContentUploadRoute(Route):
     async def execute(
             self,
             group_name: str,
-            upload_file: UploadFile,
-            file_metadata: Optional[dict] = None
+            file: ContentUploadConfig,
     ) -> Optional[dict]:
         http_method: Callable = getattr(self, getattr(self, 'METHOD'))  # Do it this way for inheritance
         endpoint: str = getattr(self, 'ENDPOINT')
 
         return await http_method(
             path=f"/groups/{group_name}/content/{endpoint}",
-            files={'file': (upload_file.file_name, upload_file.file_bytes, upload_file.file_mimetype)},
-            data={'file_metadata': json.dumps(file_metadata) if file_metadata else None}
+            json=file
         )
