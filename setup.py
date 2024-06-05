d834bda8513c42a79f9187a745ec5102bd4bd3a7
isaackogan <isaacikogan@gmail.com>
2024-06-05 14:36:23 -0400

    Update SDK Versioning

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 setuptools.setup(
     name="CriadexSDK",
     packages=setuptools.find_packages(),
-    version="1.2.0",
+    version="1.2.1",
     description="Criadex SDK Client",
     author="Isaac Kogan",
     author_email="koganisa@yorku.ca",
